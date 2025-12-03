4d8cd45a0c4f104eeea33cdc1ac0899803b7b879
Maarten Oosterhoff <maarten.oosterhoff@gmail.com>
2025-12-03 09:57:39 +0100

    Add project files.

---
+++ AdventOfCode2025.Puzzles/Puzzle1/Runner.cs
@@ -0,0 +1,76 @@
+﻿using AdventOfCode2025.Puzzles.Utils;
+using FluentAssertions;
+using Xunit;
+
+namespace AdventOfCode2025.Puzzles.Puzzle1;
+
+public class Runner
+{
+    [Theory]
+    [InlineData("TestInput", 3)]
+    [InlineData("Input", 1147)]
+    public void RunAlpha(string filename, int expected)
+    {
+        var actual = Execute(filename);
+        actual.Should().Be(expected);
+    }
+
+    //[Theory]
+    //[InlineData("TestInput", 31)]
+    //[InlineData("Input", 23177084)]
+    //public void RunBeta(string filename, int expected)
+    //{
+    //    var (points1, points2) = Execute(filename);
+    //    var map = points2.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+    //    var sum = 0;
+
+    //    foreach (var point1 in points1)
+    //    {
+    //        if (map.TryGetValue(point1, out var scoreIncreaser))
+    //        {
+    //            sum += point1 * scoreIncreaser;
+    //        }
+    //    }
+
+    //    sum.Should().Be(expected);
+    //}
+
+    private static int Execute(string filename)
+    {
+        var lines = EmbeddedResourceReader.Read<Runner>(filename);
+
+        var pos = 50;
+        var count = 0;
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                continue;
+            }
+
+            if (line[0] == 'L')
+            {
+                pos -= int.Parse(line.Substring(1));
+            }
+            else if (line[0] == 'R')
+            {
+                pos += int.Parse(line.Substring(1));
+            } else
+            {
+                throw new NotImplementedException();
+            }
+
+                pos = pos % 100;
+
+            if (pos == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+
+    }
+}
---
+++ AdventOfCode2025.Puzzles/Utils/EmbeddedResourceReader.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace AdventOfCode2025.Puzzles.Utils;
+
+internal class EmbeddedResourceReader
+{
+    public static string[] Read<T>(string filename)
+    {
+        var folder = typeof(T).Namespace!.Replace("AdventOfCode2025.Puzzles.", string.Empty);
+
+        var lines = Read(folder, filename);
+
+        return lines
+            .Replace(Environment.NewLine, "\n")
+            .Replace("\r", "\n")
+            .Split("\n")
+            .ToArray();
+    }
+
+    private static string Read(string folder, string filename)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = $"{typeof(EmbeddedResourceReader).Namespace?.Replace(".Utils", "")}.{folder}.{filename}.txt";
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        using var reader = new StreamReader(stream!);
+        var result = reader.ReadToEnd();
+        return result;
+    }
+}
