2e48bf825f411f1fc817b6bc142ded78fcae114f
Maarten Oosterhoff <maarten.oosterhoff@gmail.com>
2025-12-11 09:54:15 +0100

    ✨ Finished puzzle 8a.

--- AdventOfCode2025.Puzzles/Puzzle8/Runner.cs
+++ AdventOfCode2025.Puzzles/Puzzle8/Runner.cs
@@ -1,17 +1,16 @@
 ﻿using AdventOfCode2025.Puzzles.Utils;
 using FluentAssertions;
-using System.Collections.Concurrent;
 
 namespace AdventOfCode2025.Puzzles.Puzzle8;
 
 public class Runner
 {
     [Test]
-    [Arguments("TestInput", 40)]
-    [Arguments("Input", 0)]
-    public void RunAlpha(string filename, long expected)
+    [Arguments("TestInput", 40, 10)]
+    [Arguments("Input", 46398, 1000)]
+    public void RunAlpha(string filename, int expected, int connectCount)
     {
-        var actual = Execute(filename);
+        var actual = Execute(filename, connectCount);
         actual.Should().Be(expected);
     }
 
@@ -22,14 +21,14 @@ public class Runner
     //{
     //}
 
-    private static long Execute(string filename)
+    private static int Execute(string filename, int connectCount)
     {
         var lines = EmbeddedResourceReader.Read<Runner>(filename);
 
         var junctionBoxes = lines.Select(JunctionBox.Create).ToArray();
         var permutations = Permutate(junctionBoxes).OrderBy(x => x.Distance).ToArray();
         var circuits = new List<Circuit>();
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < connectCount; i++)
         {
             var perm = permutations[i];
             var leftCircuit = circuits.FirstOrDefault(x => x.Contains(perm.Left));
@@ -46,15 +45,27 @@ public class Runner
             {
                 leftCircuit.Add(perm.Right);
             }
-            else
+            else if (leftCircuit is not null && rightCircuit is not null)
             {
-                // Both are not null
-                // TODO: Combine circuits
+                if (leftCircuit.Equals(rightCircuit))
+                {
+                    // Both are not null but equal
+                }
+                else
+                {
+                    // Both are not null but different
+                    leftCircuit.Combine(rightCircuit);
+                    rightCircuit.Clear();
+                }
             }
-
         }
 
-        return 0;
+        return circuits
+            .Where(x => x.Size != 0)
+            .Select(x => x.Size)
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Aggregate((total, next) => total * next);
     }
 
     private static List<Connection> Permutate(JunctionBox[] input)
@@ -102,6 +113,13 @@ public class Runner
 
         public void Add(JunctionBox junctionBox) => JunctionBoxes.Add(junctionBox);
 
+        public void Combine(Circuit circuit) => JunctionBoxes.AddRange(circuit.JunctionBoxes);
+
         public int Size => JunctionBoxes.Count;
+
+        public void Clear()
+        {
+            JunctionBoxes.Clear();
+        }
     }
 }
